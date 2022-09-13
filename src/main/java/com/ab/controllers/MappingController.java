c0ff9aa7957916116a78dc7c2519d9f5f9a3b1d9
Your Name <pietroannobil@Pietros-MacBook-Air.local>
2022-09-13 17:39:31 +0100

    log/reg style

--- src/main/java/com/ab/controllers/MappingController.java
+++ src/main/java/com/ab/controllers/MappingController.java
@@ -1,14 +1,23 @@
 package com.ab.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ab.models.Customer;
+import com.ab.servicies.CustomerService;
 
 
 @Controller
 public class MappingController {
 	
-	
+	@Autowired 
+	private CustomerService customerService;
 	
 	@GetMapping("/stocks/chart/{stockName}/{stockRegion}")
 	public String chart(@PathVariable("stockName") String stockName,@PathVariable("stockRegion") String stockRegion) {
@@ -22,5 +31,31 @@ public class MappingController {
 		
 		return "order_book"; 
 	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login"; 
+	}
+	
+	//@GetMapping("/Register")
+    @RequestMapping(value="/register", method = RequestMethod.GET)
+    public String showRegistrationForm (){
+    	return "register";
+    }
+    
+
+    
+    @PostMapping("/registerCustomer")
+    public String registerProcess(@ModelAttribute Customer c) {
+    	
+    	Customer registeredCustomer = this.customerService.registerCustomer(c);
+    	if(registeredCustomer != null) {
+    		
+    		return "register_success";
+	   }
+    	else {
+    		return "reg_faiure";
+    	}
+    }
 
 }

45a01c2f38675ba7b679ff54cae6aba29aa48e93
Your Name <pietroannobil@Pietros-MacBook-Air.local>
2022-09-14 23:09:08 +0100

    a

--- src/main/java/com/ab/controllers/CustomerController.java
+++ src/main/java/com/ab/controllers/CustomerController.java
@@ -3,6 +3,6 @@ package com.ab.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public interface CustomerController {
+public class CustomerController {
 
 }

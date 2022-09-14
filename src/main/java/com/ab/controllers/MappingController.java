ae5351d9d2e4e4b25e725a9b4357af0470cf1444
sach7691 <sach7691@gmail.com>
2022-09-14 12:43:59 +0100

    kishore did session

--- src/main/java/com/ab/controllers/MappingController.java
+++ src/main/java/com/ab/controllers/MappingController.java
@@ -1,19 +1,24 @@
 package com.ab.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
+
 import com.ab.models.Customer;
 import com.ab.models.Stock;
 import com.ab.servicies.CustomerService;
 
-
+@SessionAttributes({"session_customer"})
 @Controller
 public class MappingController {
 	
@@ -24,6 +29,13 @@ public class MappingController {
 	public String chart() {			
 		return "stock_chart"; 
 	}
+	
+	@ModelAttribute
+	public Customer customer() {
+		
+		return new Customer();
+	}
+		
 	
 	@GetMapping("/stocks/chart/{stockId}/{stockName}/{stockQuantity}/{stockRegion}")
 	public ModelAndView chart(@PathVariable("stockId") int stockId,@PathVariable("stockName") String stockName,@PathVariable("stockQuantity") int stockQuantity,@PathVariable("stockRegion") String stockRegion) {
@@ -62,9 +74,12 @@ public class MappingController {
     
     
     @PostMapping("/registerCustomer")
-    public String registerProcess(@ModelAttribute Customer c) {
+    public String registerProcess(@ModelAttribute Customer c, Model model) {
     	
     	Customer registeredCustomer = this.customerService.registerCustomer(c);
+    	
+    	model.addAttribute("session_customer", registeredCustomer);
+    	
     	if(registeredCustomer != null) {
     		
     		return "register_success";
@@ -72,6 +87,15 @@ public class MappingController {
     	else {
     		return "reg_faiure";
     	}
+    }
+      
+    @GetMapping("/logout")
+    public String logout(SessionStatus session) {
+    	
+    	session.setComplete();
+    	
+    	return "stock_list";
+    	
     }
 
 
--- src/main/java/com/ab/controllers/OrderBookController.java
+++ src/main/java/com/ab/controllers/OrderBookController.java
@@ -1,9 +1,17 @@
 package com.ab.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.ab.models.Customer;
+import com.ab.models.OrderBook;
 import com.ab.servicies.OrderBookService;
 
+@SessionAttributes({"session_customer"})
 @RestController
 public class OrderBookController {
 	
@@ -11,18 +19,20 @@ public class OrderBookController {
 	@Autowired
 	private OrderBookService orderBookService;
 	
-//	@PostMapping("/stocks/orderbook/{customerId}/{orderPrice}/{orderQuantity}/{orderType}/{stockId}")
-//	public OrderBook newOrder(@PathVariable("customerId") int customerId,@PathVariable("orderPrice") double orderPrice,@PathVariable("orderQuantity") int orderQuantity,@PathVariable("orderType") String orderType,@PathVariable("stockId") int stockId) {
-//		
-//		OrderBook or = new OrderBook(orderType, orderQuantity, orderPrice, customerId, stockId);
-//		System.out.println(or);
-//		return orderBookService.newOrder(or);
-//		
-//	}
+	@PostMapping("/stocks/orderbook/{customerId}/{orderPrice}/{orderQuantity}/{orderType}/{stockId}")
+	public OrderBook newOrder(@PathVariable("customerId") int customerId,@PathVariable("orderPrice") double orderPrice,@PathVariable("orderQuantity") int orderQuantity,@PathVariable("orderType") String orderType,@PathVariable("stockId") int stockId) {
+		
+		OrderBook or = new OrderBook(orderType, orderQuantity, orderPrice, customerId, stockId);
+		System.out.println(or);
+		return orderBookService.newOrder(or);
+		
+	}
 	
 //	@PostMapping("/stocks/orderbook")
-//	public OrderBook newOrder(@RequestBody OrderBook or) {
-//	//	OrderBook or = new OrderBook(orderPrice,orderQuantity,orderType);
+//	public OrderBook newOrder(@ModelAttribute OrderBook or, @ModelAttribute("session_customer") Customer customer) {
+//		
+//		
+//	    OrderBook or = new OrderBook(or.ge,orderQuantity,orderType,customer.getCustomerId());
 //		System.out.println(or);
 //		return orderBookService.newOrder(or);
 //		

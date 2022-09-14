d6e988fcccfdfd9f4aec93c13a0a8122884f749f
Austin Li <austin@192.168.1.102>
2022-09-14 16:00:10 +0100

    update

--- src/main/java/com/ab/controllers/CustomerController.java
+++ src/main/java/com/ab/controllers/CustomerController.java
@@ -1,8 +1,37 @@
 package com.ab.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public interface CustomerController {
+import com.ab.models.Customer;
+import com.ab.servicies.CustomerService;
 
+@RestController
+public class CustomerController {
+	
+	@Autowired 
+	private CustomerService customerService;
+	
+	
+	  @PostMapping("/registerCustomer")
+	    public String registerProcess(@ModelAttribute Customer c, Model model) {
+	    	
+	    	Customer registeredCustomer = this.customerService.registerCustomer(c);
+	    	
+	    	model.addAttribute("session_customer", registeredCustomer);
+	    	
+	    	if(registeredCustomer != null) {
+	    		
+	    		return "register_success";
+		   }
+	    	else {
+	    		return "reg_faiure";
+	    		
+	   @PostMapping("/login")
+	   
+	    	}
+	    }
 }

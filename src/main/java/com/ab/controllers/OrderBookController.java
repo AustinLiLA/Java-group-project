ea12cbe115df5745cc3080889493fd92182e0604
sach7691 <sach7691@gmail.com>
2022-09-20 14:56:24 +0100

    entered details to orderbook

--- src/main/java/com/ab/controllers/OrderBookController.java
+++ src/main/java/com/ab/controllers/OrderBookController.java
@@ -4,9 +4,11 @@ import java.lang.annotation.Repeatable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,30 +37,69 @@ public class OrderBookController {
 		
 	} */
 	
-	@PostMapping("/stocks/orderbook")
-	public OrderBook newOrder(@ModelAttribute OrderBook or, @ModelAttribute("session_customer") Customer customer, @ModelAttribute("session_stock") List<Stock> stock) {
-		
-		
-	    OrderBook ob = new OrderBook(stock.get(0).getStockId(), customer.getCustomerId(),or.getOrderType(),or.getOrderPrice(), or.getOrderQuantity());
-		System.out.println(ob);
-		return orderBookService.newOrder(ob);
-		
-	}
+//	@PostMapping("/stocks/orderbook")
+//	public OrderBook newOrder(@RequestParam("order") String orderType,@RequestParam("price") double price,@RequestParam("quantity") int quantity,@ModelAttribute("session_customer") Customer customer, @ModelAttribute("session_stock") List<Stock> stock) {
+//		
+//		 OrderBook ob = null;
+//		 
+//		 OrderBook st = null;
+//		 
+//		for (int i = 0; i < stock.size(); i++) {
+//			
+//	     ob = new OrderBook(stock.get(i).getStockId(), customer.getCustomerId(),orderType,price,quantity);
+//	    			
+//	    orderBookService.newOrder(ob);
+//	    
+//	    st = new OrderBook();
+//			
+//		st.setCustomerId(customer.getCustomerId());
+//			
+//		st.setStockId(stock.get(i).getStockId());
+//			
+//		st.setOrderType(orderType);
+//			
+//		st.setOrderQuantity(quantity);
+//			
+//		st.setOrderPrice(price);
+//		
+//		}
+//		
+//	    
+//		return st;
+//	}
 
-	@GetMapping("/stocks/orderbook/{customerId}/{orderPrice}/{orderQuantity}/{orderType}/{stockId}")
-	public ModelAndView allOrderBooks(){
+//	@GetMapping("/stocks/orderbook/{customerId}/{orderPrice}/{orderQuantity}/{orderType}/{stockId}")
+//	public ModelAndView allOrderBooks(){
+//		
+//		ModelAndView mv = new ModelAndView();
+//		
+//		List<OrderBook> orderBookList = orderBookService.displayOrderBooks();		
+//		
+//		mv.addObject("orderBookList",orderBookList); 
+//		
+//		mv.setViewName("order_book");
+//		
+//		return mv; // represent "view name"/jsp file name
+//		
+//	}
+	
+    @GetMapping("/stocks/orderbook")
+    public ModelAndView getOrderBook(@ModelAttribute OrderBook orderBook, Model model) {
+
+    	ModelAndView mv = new ModelAndView();
+    	 	 
+        List<OrderBook> orderBookList =  orderBookService.displayOrderBooks();
 		
-		ModelAndView mv = new ModelAndView();
-		
-		List<OrderBook> orderBookList = orderBookService.displayOrderBooks();		
-		
+		System.out.println(orderBookList);
 		mv.addObject("orderBookList",orderBookList); 
 		
 		mv.setViewName("order_book");
-		
-		return mv; // represent "view name"/jsp file name
-		
-	}
+	
+		return mv; 
+    		
+    	
+    }
+
 	
 	
 	
